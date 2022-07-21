64721e7e686e7893c67dcd41c918adf884a1e479
nicolekarakin <nicole.karakin@gmail.com>
2022-07-21 20:10:03 +0200

    added axios get

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 // import './Exp1.css';
+//import Exp1 from "./components/reactold/Exp1";
 import MHeader from "./components/MHeader";
 import Gallery from "./components/Gallery";
-import Exp1 from "./components/reactold/Exp1";
-import data from "./galery.json" ;
+//import data from "./galery.json" ;
 import {CardType} from "./types/CardType";
+import axios from "axios";
 
 function App() {
     //enforce datatype with as ...
     //const allCards:CardType[]=data as CardType[]
     // @ts-ignore
-    const allCards:CardType[]=data
+    //const allCards:CardType[]=data
+
+    const [allCards, setAllCards] = useState<CardType[]>([])
+    useEffect(() => {
+        getCharacters()
+    }, [])
+
+    const getCharacters = () => {
+        axios.get("https://rickandmortyapi.com/api/character")
+            .then((response) => {
+                return response.data
+            })
+            .then((data) => {
+                setAllCards(data.results)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }
+
   return (
     <div className="App">
       <header className="App-header">
